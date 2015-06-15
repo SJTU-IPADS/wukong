cc924c5803911c2fa1ba8dc510f82db7d82828ea
shijiaxin <shijiaxin.cn@gmail.com>
2015-06-15 19:05:21 +0800

    initial

---
+++ test_graph.cpp
@@ -0,0 +1,137 @@
+#include "graph.h"
+#include "traverser.h"
+
+int main(int argc, char * argv[])
+{
+	if(argc !=2)
+	{
+		printf("usage:./test_graph dir\n");
+		return -1;
+	}
+	graph g(argv[1]);
+	//Current I cannot correct parse the ontology file.
+	//and not all the subClass are list in the file.
+	//So I manually insert it and make it reasonable
+
+	//Course
+	g.insert_subclass("<ub#GraduateCourse>","<ub#Course>");
+	//student
+	g.insert_subclass("<ub#GraduateStudent>","<ub#Student>");
+	g.insert_subclass("<ub#UndergraduateStudent>","<ub#Student>");
+	//professor
+	g.insert_subclass("<ub#FullProfessor>","<ub#Professor>");
+	g.insert_subclass("<ub#AssistantProfessor>","<ub#Professor>");
+	g.insert_subclass("<ub#AssociateProfessor>","<ub#Professor>");
+	//Faculty
+	g.insert_subclass("<ub#Professor>","<ub#Faculty>");
+	g.insert_subclass("<ub#Lecturer>","<ub#Faculty>");
+
+	//Person
+	g.insert_subclass("<ub#Student>","<ub#Person>");
+	g.insert_subclass("<ub#Faculty>","<ub#Person>");
+
+	g.insert_subclass("<ub#TeachingAssistant>","<ub#Person>");
+	g.insert_subclass("<ub#ResearchAssistant>","<ub#Person>");
+
+
+
+	g.print_ontology_tree();
+
+	{
+		// test
+		traverser t(g);
+		t.get_all_subtype("<ub#University>")
+			.LoadNeighbors("in","<http://www.w3.org/1999/02/22-rdf-syntax-ns#type>")
+			.print_count();
+	}
+
+	{
+		// Query 1
+		traverser t(g);
+		t.lookup("<http://www.Department0.University0.edu/GraduateCourse0>")
+			.LoadNeighbors("in","<ub#takesCourse>")
+			.is_subclass_of("<ub#GraduateStudent>")
+			.print_count();
+	}
+
+	{
+		// Query 2 TODO
+		traverser t(g);
+		t.print_count();
+	}
+	
+	{
+		// Query 3
+		traverser t(g);
+		t.lookup("<http://www.Department0.University0.edu/AssistantProfessor0>")
+			.LoadNeighbors("in","<ub#publicationAuthor>")
+			.is_subclass_of("<ub#Publication>")
+			.print_count();
+	}
+	
+	{
+		// Query 4
+		traverser t(g);
+		t.lookup("<http://www.Department0.University0.edu>")
+			.LoadNeighbors("in","<ub#worksFor>")
+			.is_subclass_of("<ub#Professor>")
+			//.print_property()
+			.print_count();
+	}
+
+	{
+		// Query 5 
+		traverser t(g);
+		t.lookup("<http://www.Department0.University0.edu>")
+			.LoadNeighbors("in","<ub#memberOf>")
+			.print_count();
+	}
+
+	{
+		// Query 6 
+		traverser t(g);
+		t.get_all_subtype("<ub#Student>")
+			.LoadNeighbors("in","<http://www.w3.org/1999/02/22-rdf-syntax-ns#type>")
+			.print_count();
+	}
+
+	{
+		// Query 7
+		traverser t(g);
+		t.lookup("<http://www.Department0.University0.edu/AssociateProfessor0>")
+			.LoadNeighbors("out","<ub#teacherOf>")
+			.is_subclass_of("<ub#Course>")
+			.LoadNeighbors("in","<ub#takesCourse>")
+			.is_subclass_of("<ub#Student>")
+			//.print_property()			
+			.print_count();
+	}
+
+	{
+		// Query 8
+		traverser t(g);
+		t.lookup("<http://www.University0.edu>")
+			.LoadNeighbors("in","<ub#subOrganizationOf>")
+			.is_subclass_of("<ub#Department>")	
+			.LoadNeighbors("in","<ub#memberOf>")
+			.is_subclass_of("<ub#Student>")
+			.print_count();
+	}
+
+	{
+		// Query 9 TODO
+		traverser t(g);
+		t.print_count();
+	}
+
+	{
+		// Query 10 
+		traverser t(g);
+		t.lookup("<http://www.Department0.University0.edu/GraduateCourse0>")
+			.LoadNeighbors("in","<ub#takesCourse>")
+			.is_subclass_of("<ub#Student>")
+			.print_count();
+	}
+
+    return 0;
+}
---
+++ traverser.h
@@ -0,0 +1,110 @@
+#pragma once
+
+#include "graph.h"
+class traverser{
+	
+	graph& g;
+	
+public:
+	unordered_set<int> ids;
+	traverser(graph& gg):g(gg){
+	} 
+	traverser& print_count(){
+		cout<<ids.size()<<endl;
+		return *this;
+	}
+	traverser& lookup(string subject){
+		unordered_set<int> empty;
+		ids=empty;
+		ids.insert(g.subject_to_id[subject]);
+		return *this;
+	}
+	traverser& LoadNeighbors(string dir,string predict){
+		unordered_set<int> neighbors;
+		int	predict_id=g.predict_to_id[predict];
+
+		for (auto i : ids){
+			if(dir =="in" || dir == "all"){
+				for (auto row : g.vertex_array[i].in_edges){
+					if(predict_id==row.predict){
+						neighbors.insert(row.vid);
+					}
+				}
+			}
+			if(dir =="out" || dir == "all"){
+				for (auto row : g.vertex_array[i].out_edges){
+					if(predict_id==row.predict){
+						neighbors.insert(row.vid);
+					}
+				}
+			}
+		}
+		ids=neighbors;
+		return *this;
+	}
+	traverser& get_all_subtype(string target){
+		int target_id=g.subject_to_id[target];
+		ids=g.ontology_array.get_all_subtype(target_id);
+		return *this;
+	}
+	traverser& is_subclass_of(string target){
+		unordered_set<int> left_ids;
+
+		int predict_id=g.predict_to_id["<http://www.w3.org/1999/02/22-rdf-syntax-ns#type>"];
+		int target_id=g.subject_to_id[target];
+
+		for (auto i : ids){
+			for (auto row : g.vertex_array[i].out_edges){
+				if(predict_id==row.predict && 
+							g.ontology_array.is_subtype_of(row.vid,target_id)){
+					left_ids.insert(i);
+					break;
+				}
+			}
+		}
+		ids=left_ids;
+		return *this;
+	}
+	// traverser& filter(string dir,string predict,string target){
+	// 	unordered_set<int> left_ids;
+
+	// 	int predict_id=g.predict_to_id[predict];
+	// 	int vid=g.subject_to_id[target];
+
+	// 	for (auto i : ids){
+	// 		if(dir =="in" || dir == "all"){
+	// 			for (auto row : g.vertex_array[i].in_edges){
+	// 				if(predict_id==row.predict && vid==row.vid){
+	// 					left_ids.insert(i);
+	// 					break;
+	// 				}
+	// 			}
+	// 		}
+	// 		if(dir =="out" || dir == "all"){
+	// 			for (auto row : g.vertex_array[i].out_edges){
+	// 				if(predict_id==row.predict && vid==row.vid){
+	// 					left_ids.insert(i);
+	// 					break;
+	// 				}
+	// 			}
+	// 		}
+	// 	}
+	// 	ids=left_ids;
+	// 	return *this;
+	// }
+
+	traverser& print_property(){
+		for (auto i : ids){
+			cout<<g.id_to_subject[i]<<endl;
+			for (auto j : g.vertex_array[i].propertys){
+				cout<<"\t"<<g.id_to_predict[j.property]<<"\t"<<j.value<<endl;
+			}
+			for (auto row : g.vertex_array[i].out_edges){
+				cout<<"\t"<<g.id_to_predict[row.predict]<<"\t"<<g.id_to_subject[row.vid]<<endl;
+			}
+		}
+		return *this;
+	}
+
+
+};
