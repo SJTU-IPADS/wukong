eb1ef8f568304a749eaf2d587bcdb0f466130393
shijiaxin <shijiaxin.cn@gmail.com>
2015-09-14 18:33:20 +0800

    refine micro_bench code

--- graph-store-distributed/micro_bench.cpp
+++ graph-store-distributed/micro_bench.cpp
@@ -24,10 +24,15 @@ void pin_to_core(size_t core) {
   CPU_SET(core , &mask);
   int result=sched_setaffinity(0, sizeof(mask), &mask);  
 }
-#define NUM_SENDER 4
-#define NUM_RECVER 1
-#define NUM_THREAD (NUM_RECVER+NUM_SENDER)
+// #define NUM_SENDER 4
+// #define NUM_RECVER 1
+// #define NUM_THREAD (NUM_RECVER+NUM_SENDER)
 
+int NUM_SENDER;
+int NUM_RECVER;
+int NUM_THREAD;
+int batch_factor;
+int extra_work;
 void* Run(void *ptr) {
   struct Thread_config *config = (struct Thread_config*) ptr;
   pin_to_core(socket_0[config->id]);
@@ -50,15 +55,25 @@ void* Run(void *ptr) {
   	timer t2;
   	while(true){
   		string tmp=node->Recv();
+      for(int times=0;times<extra_work;times++){
+        for(int i=3;i<tmp.size();i++){
+          tmp[i]++;
+        }
+      }
   		int mid=tmp[0];
   		int tid=tmp[1];
   		int count=tmp[2];
   		if(count==10){
+        node->Send(mid, tid, tmp);
   			end_num++;
   			if(end_num==10000){
   				end_num=0;
 	  			t2.reset();
-	  			cout<<endl<<"finished in "<<t2.diff(t1)<<" ms"<<endl;
+          if(config->world->rank()==0 && config->id==0)
+	  			  cout<<NUM_SENDER<<"\t"<<NUM_RECVER <<"\t"
+                <<batch_factor<<"\t"<<extra_work <<"\t"
+                <<t2.diff(t1)<<" ms"<<endl;
+          //cout<<"("<<config->world->rank()<<","<<config->id<<") finished in "<<t2.diff(t1)<<" ms"<<endl;
 	  			t1.reset();
   			}
   		}
@@ -72,9 +87,13 @@ void* Run(void *ptr) {
   	sleep(1);	
 	timer t1;
 
+  for(int i=0;i<batch_factor;i++){
+    node->Send(rand_r(&seed)%config->world->size(), rand_r(&seed)%NUM_RECVER, str);
+  }
 	for(int i=0;i<1000*10000;i++){
-		node->Send(i%config->world->size(), rand_r(&seed)%NUM_RECVER, str);
-		usleep(1);
+    node->Recv();
+		node->Send(rand_r(&seed)%config->world->size(), rand_r(&seed)%NUM_RECVER, str);
+		//usleep(1);
 	}
 	timer t2;
 
@@ -85,6 +104,15 @@ void* Run(void *ptr) {
 
 int main(int argc, char * argv[])
 {
+  if(argc!=5){
+    printf("Wrong parameters\n");
+    exit(0);
+  }
+  NUM_SENDER=atoi(argv[1]);
+  NUM_RECVER=atoi(argv[2]);
+  batch_factor=atoi(argv[3]);
+  extra_work=atoi(argv[4]);
+  NUM_THREAD=NUM_SENDER+NUM_RECVER;
 
 	boost::mpi::environment env(argc, argv);
 	boost::mpi::communicator world;
@@ -103,7 +131,7 @@ int main(int argc, char * argv[])
 	uint64_t *local_buffer = (uint64_t *)rdma->GetMsgAddr(0);
   	uint64_t start_addr=0;
   	//rdma->RdmaRead(0,(world.rank()+1)%world.size() ,(char *)local_buffer,100,start_addr);
-  	//cout<<"Fucking OK"<<endl;
+    //cout<<"Fucking OK"<<endl;
 
 	Thread_config *configs = new Thread_config[NUM_THREAD];
   	pthread_t     *thread  = new pthread_t[NUM_THREAD];
