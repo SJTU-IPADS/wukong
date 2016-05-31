f1b9c4ccab800f50a4517df32a6a45cc2e425929
RongChen@SJTU <rongchen@sjtu.edu.cn>
2016-05-31 22:13:37 +0800

    generalize scripts

--- tools/sync.sh
+++ tools/sync.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-root_dir=/home/sjx/graph-query/
+root_dir=${WUKONG_ROOT}/
 
 
 cat mpd.hosts | while read machine
 do
 	#Don't copy things like Makefile, *.o, etc.
-	rsync -rtuv --exclude=deps/* --include=build/wukong* --exclude=build/* --exclude=.git   ${root_dir} ${machine}:${root_dir}
+	rsync -rtuv --exclude=deps/* --include=build/wukong* --exclude=build/* --exclude=.git $r{oot_dir} ${machine}:${root_dir}
 done
