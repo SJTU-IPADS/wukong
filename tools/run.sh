e6bb5aa9d1a2ca7821b974498c31b0aecade98a8
RongChen@SJTU <rongchen@sjtu.edu.cn>
2016-10-09 14:05:07 +0800

    minor run.sh

--- tools/run.sh
+++ tools/run.sh
@@ -1,4 +1,13 @@
 #!/bin/sh
+#
+# Configure wukong at the file 'config'
+# List all nodes at the file 'mpd.host'
+# Share the input RDF data among all nodes through dfs (e.g., NFS)
 
+# Standalone Mode (w/o mpi):
+# ../build/wukong config
+
+# Distributed Mode (w/ mpi):
 # NOTE: the hostfile of of mpiexec must match that of wukong (i.e., mpd.hosts)
 /usr/bin/mpiexec -x LD_LIBRARY_PATH -hostfile mpd.hosts -n $1 ../build/wukong config mpd.hosts
+
