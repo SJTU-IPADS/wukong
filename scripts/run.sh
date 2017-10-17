3193cb5ffcd5e42de378725253b557d4981dbb3f
Rong Chen <rongchen@sjtu.edu.cn>
2017-10-17 10:47:54 +0800

    sweep comments

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,12 +1,9 @@
 #!/bin/sh
 #
-# Configure wukong at the file 'config'
-# List all nodes at the file 'mpd.host'
-# Share the input RDF data among all nodes through DFS (e.g., NFS or HDFS)
+# 1. List all nodes at the file 'mpd.host'
+# 2. Share the input RDF data among all nodes through DFS (e.g., NFS or HDFS)
+# 3. Configure wukong at the file 'config'
+# 4. Edit the file 'core.bind' to control the thread binding.
 
-# Standalone Mode (w/o mpi):
-# ../build/wukong config mpd.hosts -b core.bind
-
-# Distributed Mode (w/ mpi):
 # NOTE: the hostfile of mpiexec must match that of wukong (i.e., mpd.hosts)
 ${WUKONG_ROOT}/deps/openmpi-1.6.5-install/bin/mpiexec -x CLASSPATH -x LD_LIBRARY_PATH -hostfile mpd.hosts -n $1 ../build/wukong config mpd.hosts -b core.bind
