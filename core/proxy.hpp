3cbbfbce40c80c12f11ed4fb04ce8853c1503264
ljq13 <ljq2017@sjtu.edu.cn>
2018-01-05 20:37:00 +0800

    fix unstoppable bug with gzh

--- core/proxy.hpp
+++ core/proxy.hpp
@@ -391,6 +391,7 @@ public:
 
 		// recieve all replies to calculate the tail latency
 		while (recv_cnt < send_cnt) {
+			sweep_msgs();
 			request_or_reply r;
 			if (tryrecv_reply(r)) {
 				recv_cnt ++;
