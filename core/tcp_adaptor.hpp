4d4bb490ae785e3fbeea6c3b1800d51a69f8f388
Rong Chen <rongchen@sjtu.edu.cn>
2018-06-28 13:11:07 +0800

    rollback to zeromq-based tcp-adpator. The current nanomsg-based implementation has some performance issue.

--- core/tcp_adaptor.hpp
+++ core/tcp_adaptor.hpp
@@ -22,7 +22,7 @@
 
 #pragma once
 
-//#include <zmq.hpp>
+#include <zmq.hpp>
 #include <unistd.h>
 #include <string.h>
 #include <errno.h>
@@ -33,26 +33,21 @@
 #include <fstream>
 #include <sstream>
 
-//#include <nanomsg/nn.h>
-#include <nanomsg/nn.hpp>
-#include <nanomsg/pipeline.h>
-#include <nanomsg/tcp.h>
-
 #include <tbb/concurrent_unordered_map.h>
 
 using namespace std;
 
-
 class TCP_Adaptor {
 private:
-    typedef tbb::concurrent_unordered_map<int, nn::socket *> socket_map;
-    typedef vector<nn::socket *> socket_vector;
+    typedef tbb::concurrent_unordered_map<int, zmq::socket_t *> socket_map;
+    typedef vector<zmq::socket_t *> socket_vector;
 
     int port_base;
 
-    // The communication over nanomsg (https://nanomsg.org/), a socket library.
-    socket_vector receivers;  // static allocation
-    socket_map senders;       // dynamic allocation
+    // The communication over zeromq, a socket library.
+    zmq::context_t context;
+    socket_vector receivers;     // static allocation
+    socket_map senders;          // dynamic allocation
 
     pthread_spinlock_t *locks;
 
@@ -63,7 +58,7 @@ private:
 public:
 
     TCP_Adaptor(int sid, string fname, int num_threads, int port_base)
-        : port_base(port_base) {
+        : port_base(port_base), context(1) {
 
         ifstream hostfile(fname);
         string ip;
@@ -72,24 +67,10 @@ public:
 
         receivers.resize(num_threads);
         for (int tid = 0; tid < num_threads; tid++) {
-            char address[128] = "";
-            snprintf(address, 128, "tcp://*:%d", port_base + port_code(sid, tid));
-
-            try {
-                receivers[tid] = new nn::socket(AF_SP, NN_PULL);
-
-                /// NN_RCVMAXSIZE: Maximum message size that can be received, in bytes.
-                /// Negative value means that the received size is limited only by available
-                /// addressable memory. The default type is 1024KB (too small for WUKONG)
-                int limit = -1;
-                receivers[tid]->setsockopt(NN_SOL_SOCKET, NN_RCVMAXSIZE, &limit, sizeof(limit));
-
-                receivers[tid]->bind(address);
-            } catch (nn::exception e) {
-                logstream(LOG_FATAL) << "Failed to init a recv-side socket on " << address
-                                     << " (" << e.what() << ")" << LOG_endl;
-                assert(false);
-            }
+            receivers[tid] = new zmq::socket_t(context, ZMQ_PULL);
+            char address[32] = "";
+            snprintf(address, 32, "tcp://*:%d", port_base + port_code(sid, tid));
+            receivers[tid]->bind(address);
         }
 
         locks = (pthread_spinlock_t *)malloc(sizeof(pthread_spinlock_t) * num_threads);
@@ -114,64 +95,42 @@ public:
     bool send(int sid, int tid, string str) {
         int pid = port_code(sid, tid);
 
-        void *msg = nn::allocmsg(str.length(), 0);
-        memcpy(msg, str.c_str(), str.length());
+        zmq::message_t msg(str.length());
+        memcpy((void *)msg.data(), str.c_str(), str.length());
 
         // FIXME: need lock or not? what to protect?
         pthread_spin_lock(&locks[tid]);
         if (senders.find(pid) == senders.end()) {
             // new socket on-demand
-            char address[128] = "";
-            snprintf(address, 128, "tcp://%s:%d", ipset[sid].c_str(), port_base + pid);
-
-            try {
-                senders[pid] = new nn::socket(AF_SP, NN_PUSH);
-                senders[pid]->connect(address);
-            } catch (nn::exception e) {
-                logstream(LOG_FATAL) << "Failed to new a send-side socket on " << address
-                                     << " (" << e.what() << ")" << LOG_endl;
-                assert(false);
-            }
+            char address[32] = "";
+            snprintf(address, 32, "tcp://%s:%d", ipset[sid].c_str(), port_base + pid);
+            senders[pid] = new zmq::socket_t(context, ZMQ_PUSH);
+            /// FIXME: check return value
+            senders[pid]->connect(address);
         }
 
-        int n = 0;
-        try {
-            n = senders[pid]->send(&msg, NN_MSG, 0);
-        } catch (nn::exception e) {
-            logstream(LOG_FATAL) << "Failed to send msg (" << e.what() << ")" << LOG_endl;
-            assert(false);
-        }
+        bool result = senders[pid]->send(msg, ZMQ_DONTWAIT);
         pthread_spin_unlock(&locks[tid]);
 
-        return (n == str.length());
+        return result;
     }
 
     string recv(int tid) {
-        try {
-            void *msg = NULL;
-            int n = receivers[tid]->recv(&msg, NN_MSG, 0);
-
-            string s((char *)msg, n);
-            nn::freemsg(msg);
-            return s;
-        } catch (nn::exception e) {
-            logstream(LOG_FATAL) << "Failed to recv msg (" << e.what() << ")" << LOG_endl;
+        zmq::message_t msg;
+        if (receivers[tid]->recv(&msg) < 0) {
+            logstream(LOG_ERROR) << "Failed to recv msg ("
+                                 << strerror(errno) << ")" << LOG_endl;
             assert(false);
         }
+
+        return string((char *)msg.data(), msg.size());
     }
 
-    bool tryrecv(int tid, string &s) {
-        try {
-            void *msg = NULL;
-            int n = receivers[tid]->recv(&msg, NN_MSG, NN_DONTWAIT);
-            if (n < 0)
-                return false;
-            s = string((char *)msg, n);
-            nn::freemsg(msg);
-            return true;
-        } catch (nn::exception e) {
-            logstream(LOG_FATAL) << "Failed to tryrecv msg (" << e.what() << ")" << LOG_endl;
-            assert(false);
-        }
+    bool tryrecv(int tid, string &str) {
+        zmq::message_t msg;
+        bool success = false;
+        if (success = receivers[tid]->recv(&msg, ZMQ_NOBLOCK))
+            str = string((char *)msg.data(), msg.size());
+        return success;
     }
 };
