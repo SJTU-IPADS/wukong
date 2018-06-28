13212ee1d34ee663e0b82dd4d75a193255cc1947
aswasn <aswasn@qq.com>
2018-06-28 22:44:12 +0800

    add gpu_utils

---
+++ utils/gpu_utils.hpp
@@ -0,0 +1,12 @@
+#include <cuda_runtime.h>
+#define CUDA_ASSERT(ans) { checkCudaResult((ans), __FILE__, __LINE__); }
+
+inline void checkCudaResult(cudaError_t code, const char *file, int line, bool abort=true)
+{
+   if (code != cudaSuccess)
+   {
+      fprintf(stderr,"CUDA_ASSERT: code:%d, %s %s:%d\n", code, cudaGetErrorString(code), file, line);
+      if (abort) assert(false);
+   }
+}
+
