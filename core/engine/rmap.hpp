c98a23e6eb5b1bb9dc51bab8380269c244c1a2c8
Rong Chen <rongchen@sjtu.edu.cn>
2018-11-30 20:24:37 +0800

    fix divide-by-zero issue, but has other correct bug and perfomrance bug

--- core/engine/rmap.hpp
+++ core/engine/rmap.hpp
@@ -48,10 +48,9 @@ public:
         // not exist
         ASSERT(internal_map.find(r.qid) == internal_map.end());
 
-        Item d;
-        d.cnt = cnt;
-        d.parent = r;
-
+        Item d = { .cnt = cnt, .parent = r, };
+        //d.cnt = cnt;
+        //d.parent = r;
         internal_map[r.qid] = d;
     }
 
@@ -64,26 +63,26 @@ public:
         SPARQLQuery::Result &part = r.result;
         d.cnt--;
 
-        // if the PatternGroup of r comes from a query's union part,
+        // if the PatternGroup comes from a query's UNION part,
         // use merge_result to put result
         if (r.pg_type == SPARQLQuery::PGType::UNION)
             whole.merge_result(part);
         else
             whole.append_result(part);
 
-        // keep inprogress
-        if (d.parent.state == SPARQLQuery::SQState::SQ_PATTERN)
-            d.reply.pattern_step = r.pattern_step;
 
-        // if r is a forked query that has done OPTIONAL query,
-        // update its parent's optional_step to avoid the parent do it again
-        if (r.pg_type != SPARQLQuery::PGType::OPTIONAL
+        // NOTE: all sub-jobs have the same pattern_step, optional_step, and union_done
+        // update parent's pattern step (progress)
+        if (d.parent.state == SPARQLQuery::SQState::SQ_PATTERN)
+            d.parent.pattern_step = r.pattern_step;
+
+        // update parent's optional_step (avoid recursive execution)
+        if (d.parent.pg_type == SPARQLQuery::PGType::OPTIONAL
                 && r.done(SPARQLQuery::SQState::SQ_OPTIONAL))
             d.parent.optional_step = r.optional_step;
 
-        // if r is a forked query that has done UNION query,
-        // update its parent's union_done to avoid the parent do it again
-        if (r.union_done)
+        // update parent's union_done (avoid recursive execution)
+        if (r.done(SPARQLQuery::SQState::SQ_UNION))
             d.parent.union_done = true;
     }
 
@@ -95,23 +94,16 @@ public:
         SPARQLQuery r = internal_map[qid].parent;
         SPARQLQuery &reply = internal_map[qid].reply;
 
-        // copy the result
-        // FIXME: implement copy construct of SPARQLQuery::Result
+        // copy metadata of result
         r.result.col_num = reply.result.col_num;
         r.result.row_num = reply.result.row_num;
         r.result.attr_col_num = reply.result.attr_col_num;
-
-        r.result.blind = reply.result.blind;
-        r.result.nvars = reply.result.nvars;
         r.result.v2c_map = reply.result.v2c_map;
-        r.result.required_vars = reply.result.required_vars;
+        // NOTE: no need to set nvars, required_vars, and blind
 
+        // copy data of result
         r.result.result_table.swap(reply.result.result_table);
         r.result.attr_res_table.swap(reply.result.attr_res_table);
-
-        // FIXME: need sync other fields or not
-        if (r.state == SPARQLQuery::SQState::SQ_PATTERN)
-            r.pattern_step = reply.pattern_step;
 
         internal_map.erase(qid);
         logstream(LOG_DEBUG) << "erase parent-qid=" << qid << LOG_endl;
