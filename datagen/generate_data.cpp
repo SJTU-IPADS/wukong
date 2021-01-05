5d267c0d7544804b91d2f82fd1e2f4ed6c942564
Xiating Xie <xiatingxie@gmail.com>
2021-01-05 13:41:24 +0800

    skip comments

--- datagen/generate_data.cpp
+++ datagen/generate_data.cpp
@@ -107,6 +107,19 @@ class Encoder {
 
     Logger<Record> logger;
 
+    // Skip strings which indicate comments in RDF/XML files.
+    const vector<string> skip_strs = 
+            {"<http://www.w3.org/2002/07/owl#Ontology>",
+             "<http://www.w3.org/2002/07/owl#imports>" };
+
+    bool skip_triple(string &sub, string &pre, string &obj) {
+        for (auto s : skip_strs) {
+            if (sub == s || pre == s || obj == s)
+                return true;
+        }
+        return false;
+    }
+
     int find_type (string str) {
         if (str.find("^^xsd:int") != string::npos
                 || str.find("^^<http://www.w3.org/2001/XMLSchema#int>") != string::npos)
@@ -260,6 +273,8 @@ class Encoder {
                 str_to_str[prekey] = object;
                 continue;
             }
+            if (skip_triple(subject, predicate, object))
+                continue;
 
             int type = 0;
             // the attr triple
@@ -298,6 +313,8 @@ class Encoder {
                         object = prev.substr(0, prev.size()-1) + lefts + '>';
                     }
                 }
+                if (skip_triple(subject, predicate, object))
+                    continue;
 
                 // add a new normal vertex (i.e., vid)
                 i64 sid = insert(normal_table, local_normal_table, subject, next_normal_id);
